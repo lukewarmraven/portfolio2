9089a75b1dd7b7b9b63fa2d1291f256dc2272099
bacquegou <quintoravenluke@gmail.com>
2026-08-02 15:53:32 +0800

    fix: adjust panel vertical positioning to use top inset
    - Replace top-0 h-screen with top-[22.47vh] bottom-0 on LeftMain and RightMain

--- components/ui/page-ui/left-main.tsx
+++ components/ui/page-ui/left-main.tsx
@@ -1,6 +1,6 @@
 export default function LeftMain({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="absolute left-[12.67vw] top-0 w-[36.75vw] h-screen">
+    <div className="absolute left-[12.67vw] top-[22.47vh] bottom-0 w-[36.75vw]">
       {children}
     </div>
   );
--- components/ui/page-ui/right-main.tsx
+++ components/ui/page-ui/right-main.tsx
@@ -1,6 +1,6 @@
 export default function RightMain({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="absolute right-[12.67vw] top-0 w-[36.75vw] h-screen">
+    <div className="absolute right-[12.67vw] top-[22.47vh] bottom-0 w-[36.75vw]">
       {children}
     </div>
   );
